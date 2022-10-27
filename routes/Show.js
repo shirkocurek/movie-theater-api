a7b58b4ec0a48eee346bfd0deef8fd762b9405df
shirkocurek <shir617@gmail.com>
2022-10-26 21:03:15 -0400

    PUT methods in Show written but not passing a test

--- routes/Show.js
+++ routes/Show.js
@@ -9,17 +9,20 @@ app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
 
-
+// GET all shows //
 router.get('/', async (req, res) => {
     const shows = await Show.findAll();
     res.json(shows);
   })
 
+// GET one show //  
 router.get('/:id', async (req, res) => {
     const foundshow = await Show.findByPk(req.params.id);
     res.json(foundshow);
   })
 
+ 
+// GET shows of a particular genre //
   router.get('/genres/:genre', async (req, res) => {
     const genre = await Show.findAll({
        where : {
@@ -30,18 +33,33 @@ router.get('/:id', async (req, res) => {
     res.json(genre);
   })
 
+// PUT update rating of a show that has been watched //
+router.put("/ratings/:rating", async (req, res) =>{
+  const rating = await Show.findAll({
+    where: {
+      rating : req.params.rating
+    }
+  })
+  res.json(await Show.findAll())
+})
+
+
+
+
+// PUT update the status of a show //
+router.put("/statuses/:status", async (req,res)=>{
+   await Show.update(req.body, {
+    where: {
+      status : req.params.status
+    }
+   })
+   res.json(await Show.findAll())
+})
   
-  
 
 
-
-
-
-
-
-
-
-  router.delete("/:id", async (req, res) =>{
+// DELETE a show //
+router.delete("/:id", async (req, res) =>{
     const showtodelete = await Show.destroy({
       where: {
         id: req.params.id
--- routes/User.js
+++ routes/User.js
@@ -10,19 +10,19 @@ app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
 
-
+// GET all users //
 router.get('/', async (req, res) => {
     const users = await User.findAll();
     res.json(users);
   })
 
+// GET one users //
 router.get('/:id', async (req, res) => {
     const founduser = await User.findByPk(req.params.id);
     res.json(founduser);
   })
 
-// GET all shows watched by a user (user id in req.params)//
-
+// GET all shows watched by a user //
 router.get('/:id/shows', async(req,res) =>{
   const showsWatched = await Show.findAll({
     where: {
@@ -32,6 +32,8 @@ router.get('/:id/shows', async(req,res) =>{
 
   res.json(showsWatched);
 })
+
+// PUT update and add a show if a user has watched it //
   
   
 
